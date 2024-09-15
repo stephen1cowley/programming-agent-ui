bafc2053c9d50524218be1cb11ce72528a0a7e80
stephen1cowley <98669276+stephen1cowley@users.noreply.github.com>
2024-09-15 15:34:22 +0100

    Reset the website upon reload

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect} from 'react';
 import './App.css';
+import { DeepChat } from 'deep-chat-react';
+import axios from 'axios';
 
 function App() {
+  const history = [
+    { role: 'ai', text: "Describe the web application you'd like me to create!" },
+  ];
+  useEffect(() => {
+    makeResetRequest()
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Programming Agent</h1>
+      <DeepChat
+        connect={{ url: process.env.REACT_APP_BACKEND_SERVER + '/api/message'}}
+        style={{ width: '90vw', height: '80vh', borderRadius: '10px' }}
+        textInput={{ placeholder: { text: 'Type here...' } }}
+        history={history}
+      />
     </div>
   );
+}
+
+async function makeResetRequest() {
+  try {
+    const response = await axios.put(process.env.REACT_APP_BACKEND_SERVER + '/api/restart');
+  } catch (error) {
+    console.error('Error making the PUT request', error)
+  }
 }
 
 export default App;
