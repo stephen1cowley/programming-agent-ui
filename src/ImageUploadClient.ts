f0ad10f830696e3a4e868e8c80d115d2fca64be5
stephen1cowley <98669276+stephen1cowley@users.noreply.github.com>
2024-09-24 12:30:57 +0100

    console debug

--- src/ImageUploadClient.ts
+++ src/ImageUploadClient.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 const uploadFile = async (file: File, uploadUrl: string) => {
     const formData = new FormData()
     formData.append("file", file)
+    console.log("Form Data:", formData.get("file"));
 
     try {
         const response = await axios.post(uploadUrl, formData, {
