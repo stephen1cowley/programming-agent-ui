50495f00039621cb656810f555da135a44c08a3f
stephen1cowley <98669276+stephen1cowley@users.noreply.github.com>
2024-09-24 10:45:37 +0100

    Removed S3 stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
 import React, {useEffect, useState} from 'react';
 
-
 import axios from 'axios';
-import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
-import Button from 'react-bootstrap/Button';
-import { ThemeProvider } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Image from 'react-bootstrap/Image';
 import Dropzone from './DragAndDrop';
 import ChatComponent from './ChatComponent'
 
@@ -19,54 +14,10 @@ const backendBaseUrl: string = "https://api.stephencowley.com"
 
 
 function App() {
-
-  // AWS S3 bucket details
-  const REGION = 'eu-west-2'; // Your bucket region
-  const BUCKET_NAME = 'my-programming-agent-img-store';
-
-  const s3 = new S3Client({
-    region: REGION,
-    credentials: {
-      accessKeyId: process.env.REACT_APP_ACCESS_KEY_ID + '', // You can use dummy keys for a public bucket
-      secretAccessKey: process.env.REACT_APP_SECRET_ACCESS_KEY + '',
-    },
-  });
-
-  const [selectedFile, setSelectedFile] = useState<File | null>(null);
-  const [uploadMessage, setUploadMessage] = useState<string>('');
-
-   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.files && event.target.files.length > 0) {
-      setSelectedFile(event.target.files[0]);
-    }
-  };
-
-  const handleUpload = async () => {
-    if (!selectedFile) {
-      alert('Please select a file first!!!');
-      return;
-    }
-
-    const uploadParams = {
-      Bucket: BUCKET_NAME,
-      Key: selectedFile.name,
-      Body: selectedFile,
-      ContentType: selectedFile.type,
-    };
-
-    try {
-      const data = await s3.send(new PutObjectCommand(uploadParams));
-      setUploadMessage(`File uploaded successfully!`);
-      console.log('Upload success:', data);
-    } catch (error) {
-      console.error('Error uploading the file:', error);
-      setUploadMessage('File upload failed.');
-    }
-  };
-
   useEffect(() => {
     makeResetRequest()
   }, [])
+
   return (
     <div className="App" data-bs-theme="dark" style={{
       backgroundColor: '#1c1c1c',
