e83be9cb71a3b6ed79e17490a646266eaab2a570
stephen1cowley <98669276+stephen1cowley@users.noreply.github.com>
2024-09-29 11:00:01 +0100

    Working sign in connected with cognito

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,24 @@ import React, {useEffect, useState} from 'react';
 
 import axios from 'axios';
 import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Dropzone from './DragAndDrop';
 import ChatComponent from './ChatComponent'
+
+import { Amplify } from 'aws-amplify';
+import awsExports from './aws-exports';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const backendBaseUrl: string = "https://api.stephencowley.com"
 
+// Configure Amplify in index file or root file
+Amplify.configure({ ...awsExports });
 
 function App() {
   useEffect(() => {
@@ -27,34 +35,49 @@ function App() {
       // justifyContent: 'center',
       // alignItems: 'center',
       }}>
-      <Container style={{
-          width: '100vw',
-          margin: 0,
-          
-        }}>
-        <Row style={{
-          height: '10vh',
-          width: '100%',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          }}
-          
-          >
-          <h3>Your Programming Agent</h3>
-        </Row>
-        <Row style={{height: '80vh'}}>
-          <Col xs={8} s={8} md={8} lg={8}>
-            <ChatComponent backendBaseUrl={backendBaseUrl} />
-          </Col>
-          <Col>
-            <Dropzone backendBaseUrl={backendBaseUrl} />
-          </Col>
-        </Row>
-        <Row style={{height: '10vh'}}>
+        <Authenticator>
+            {({ signOut, user }) => (
+                <div>
+                    <Container style={{
+                      width: '100vw',
+                      margin: 0,
+                      
+                    }}>
+                    <Row style={{
+                      height: '10vh',
+                      width: '100%',
+                      display: 'flex',
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                      }}>
+                        <Col xs={8} s={8} md={8} lg={8}>
+                        <h3>Your Programming Agent</h3>
+                        </Col>
+                        <Col>
+                        <p>Welcome {user && user.username}</p>
+                        </Col>
+                        <Col xs={2}>
+                        <Button onClick={signOut}>Sign out</Button>
+                        </Col>
+                    </Row>
+                    <Row style={{height: '80vh'}}>
+                      <Col xs={8} s={8} md={8} lg={8}>
+                        <ChatComponent backendBaseUrl={backendBaseUrl} />
+                      </Col>
+                      <Col>
+                        <Dropzone backendBaseUrl={backendBaseUrl} />
+                      </Col>
+                    </Row>
+                    <Row style={{height: '10vh'}}>
 
-        </Row>
-      </Container>
+                    </Row>
+                  </Container>
+                    
+                    
+                </div>
+            )}
+        </Authenticator>
+    
     </div>
   );
 }
---
+++ src/aws-exports.js
@@ -0,0 +1,8 @@
+const awsmobile = {
+    "aws_project_region": "eu-west-2",
+    "aws_cognito_region": "eu-west-2",
+    "aws_user_pools_id": "eu-west-2_3C5SC91YW",
+    "aws_user_pools_web_client_id": "3079slava3nqthnfh1so8vjkir"
+}
+
+export default awsmobile;
